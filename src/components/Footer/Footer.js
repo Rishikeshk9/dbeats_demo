8e21c4ef0a290b8c12922cdb5ab85d99b58b814c
Sahil Punjabi <sahilpanjab123@gmail.com>
2021-07-08 16:29:33 +0530

    dynamic footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,17 +9,19 @@ import VolumeUp from '@material-ui/icons/VolumeUp';
 import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 const PrettoSlider = withStyles({
   root: {
-    color: '#00D3FF',
+    color: '#00d3ff',
     height: 8,
   },
   thumb: {
     height: 15,
     width: 15,
     backgroundColor: '#fff',
-    border: '0.5px solid currentColor',
+    border: '2px solid currentColor',
     marginTop: -4,
     marginLeft: -12,
     '&:focus, &:hover, &$active': {
@@ -31,43 +33,41 @@ const PrettoSlider = withStyles({
     left: 'calc(-50% + 4px)',
   },
   track: {
-    height: 5,
-    borderRadius:3 ,
+    height: 4,
+    borderRadius: 2,
   },
   rail: {
-    height: 5,
-    borderRadius: 3,
+    height: 4,
+    borderRadius: 2,
   },
 })(Slider);
 
+
 const Footer = () => {
-
-  const [value, setValue] = React.useState(30);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
 
   return (
     <div className={classes.main_footer}>
-     <div className={classes.btn}>
-       <SkipPreviousIcon className={classes.skippreviousbtn}></SkipPreviousIcon>
-       <PlayCircleFilledIcon className={classes.playbtn}></PlayCircleFilledIcon>
-       <SkipNextIcon className={classes.skipnextbtn}></SkipNextIcon>
-    </div> 
-    <div className={classes.timeslider}>
-    <PrettoSlider valueLabelDisplay="auto" aria-label="pretto slider" defaultValue={20} />
+    <div className={classes.root}>
+      <Grid>
+        <Grid item xs={12}>
+        <AudioPlayer className={classes.player}
+        showSkipControls={true} showJumpControls={false}
+        customAdditionalControls={[]}
+        customVolumeControls={[]}
+        layout="stacked-reverse"
+        autoPlay
+        src="http://example.com/audio.mp3"
+        />
+        </Grid>
+      </Grid>
     </div>
-      <div className={classes.root}>
-      <Grid container spacing={2}>
+    <div className={classes.volume}>
+    <Grid container spacing={2}>
         <Grid item>
           <VolumeDown />
         </Grid>
         <Grid item xs>
         <PrettoSlider valueLabelDisplay="auto" aria-label="pretto slider" defaultValue={20} />
-        </Grid>
-        <Grid item>
-          <VolumeUp />
         </Grid>
       </Grid>
     </div>
