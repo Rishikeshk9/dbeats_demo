7e00ea3e22d04d05600401d22ac08df6188f78e8
Akash-Salvi <akashnov100@gmail.com>
2021-07-11 20:01:23 +0530

    Yes

--- Frontend/src/components/Room/Info/Info.js
+++ Frontend/src/components/Room/Info/Info.js
@@ -31,7 +31,7 @@ const Info = (props) => {
     const ref = useRef();
 
     useEffect(() => {
-        socketRef.current = io.connect("http://localhost:8000");
+        socketRef.current = io.connect("/");
         navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true }).then(stream => {
             userVideo.current.srcObject = stream;
             socketRef.current.emit("join room", roomID);
