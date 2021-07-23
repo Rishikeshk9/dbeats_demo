3a005b2aa2bd989eaa78c5b8ae7fdd0f930788cc
Sahil Punjabi <sahilpanjab123@gmail.com>
2021-07-23 15:28:49 +0530

    animation

--- Frontend/src/components/LandingPage/LandingBody/Card.js
+++ Frontend/src/components/LandingPage/LandingBody/Card.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { Card } from 'react-bootstrap';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import classes from '../Landing.module.css'
 
 const Cards = (props) => {
+
+  useEffect(()=>{
+    AOS.init({
+      offset:370,
+      duration:1500
+    })
+  },[]);
+
   return (
     <>
-<Card  className={classes.info_card} >
+<Card  className={classes.info_card} data-aos="zoom-in-up">
       <Card.Img  className="mx-auto img-shadow" id={classes.card_images} src={props.imgsrc} roundedCircle />
         <Card.Body>
           <Card.Title style={{color:'#00d3ff'}}>{props.title}</Card.Title>
