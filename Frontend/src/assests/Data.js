1b11b1b25b12ba73f40e2a66283acd46514619fd
Akash-Salvi <akashnov100@gmail.com>
2021-09-06 16:44:34 +0530

    Multi stream function

--- Frontend/src/assests/Data.js
+++ Frontend/src/assests/Data.js
@@ -1,4 +1,8 @@
 import imgback from './images/whitebackground.jpg';
+import twitch from './images/twitch.png';
+import youtube from './images/youtube.png';
+import youtube_logo from './images/youtube_logo.png';
+import twitch_logo from './images/twitch_logo.png';
 
 export const Data = [
     {
@@ -22,11 +26,16 @@ export const MultiStreamData = [
     {
         title:"Youtube",
         rtmp:"rtmp://rtmp.youtube.tv/live/",
+        image:youtube,
+        logo:youtube_logo
 
     },
     {
         title:"Twitch",
         rtmp:"rtmp://rtmp.twitch.tv/live/",
+        image:twitch,
+        logo:twitch_logo      
+
     }
 ]
 
--- Frontend/src/components/Room/Info/User_Info.js
+++ Frontend/src/components/Room/Info/User_Info.js
@@ -1,9 +1,10 @@
 import React, { Fragment, useEffect, useState } from "react";
 import classes from "./Info.module.css";
 import axios from 'axios'
-import {Button, Form, Spinner} from "react-bootstrap";
+import {Button, Form, Spinner,Modal} from "react-bootstrap";
 
 
+import {MultiStreamData} from '../../../assests/Data';
 import VideoPlayer from  '../VideoPlayer/VideoPlayer'
 
 const UserInfo = (props) => {
@@ -13,10 +14,14 @@ const UserInfo = (props) => {
     const [userStreams, setUserStreams] = useState([]);
     const user = JSON.parse(window.sessionStorage.getItem("user"));
     const [playbackUrl, setPlaybackUrl] = useState("");
-    const [twitchKey, setTwitchKey] = useState("");
+    const [StreamKey, setKey] = useState("");
     const [loader, setLoader] = useState(true);
     const [name, setName] = useState("");
+    const [modalShow, setModalShow] = useState(false);
+    const [showStreamModal, setShowStreamModal] = useState(false);
 
+    const [multiStreamValue, setMultiStreamValue] = useState({});
+    const [multiStreamConnected, setMultiStreamConnected] = useState({})
      
 
     useEffect(() => {
@@ -29,16 +34,16 @@ const UserInfo = (props) => {
 
 
     const handleChange = (e) => {
-        setTwitchKey(e.target.value);
+        e. preventDefault();
+        setKey(e.target.value);
     };
 
-
-    const createMultiStream = async () => {
+    const createMultiStream = async (props) => {
+        console.log(props)
         setLoader(false);
-        console.log("heelo",twitchKey)
         let streamData = {
             name: `${userStreams.name}`,
-            url: `rtmp://rtmp.twitch.tv/live/${twitchKey}`
+            url: `${props.rtmp}`
         }
         console.log(streamData)
         const stream = await axios({
@@ -56,11 +61,7 @@ const UserInfo = (props) => {
                 targets: [
                     {
                         id: `${patchId}`,
-                        profile: "720p",
-                    },
-                    {
-                        id: `${patchId}`,
-                        profile: "480p",
+                        profile: "source",
                     }
                 ]
             },
@@ -115,7 +116,10 @@ const UserInfo = (props) => {
                             <hr width="95%"/>
                             <div>
                                 <div className="flex">
-                                    <button variant="primary" className="bg-gradient-to-r from-dbeats-secondary-light to-dbeats-light text-white rounded font-bold px-4 py-2" type="button" onClick={createMultiStream}>
+                                    <button variant="primary" className="bg-gradient-to-r from-dbeats-secondary-light to-dbeats-light text-white rounded font-bold px-4 py-2" 
+                                        type="button" 
+                                        onClick={() => setModalShow(true)}
+                                    >
                                         Start MultiStreaming
                                     </button>
                                     <div className={classes.multistream_form_spinner}>
@@ -126,7 +130,110 @@ const UserInfo = (props) => {
                             </div>
                         </div>
                     </div>
+
+                    <div>
+                        
+                    </div>
             </div>
+
+            
+
+
+            <Modal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                size="lg"
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter" className="w-full">
+                  <div className="font-semibold text-2xl text-center">
+                    Select the MultiStream Platform
+                  </div>
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div className="flex">
+                    {MultiStreamData.map((value,index)=>{
+                        return (
+                            <div className="bg-white-200 mx-1 border-1 border-gray-300 rounded">
+                                <img src={value.image} 
+                                    alt="logo" 
+                                    className="h-32 w-auto" 
+                                    onClick={() => {
+                                        setMultiStreamValue(value);
+                                        setShowStreamModal(true);
+                                        setModalShow(false)
+                                    }}
+                                />
+                            </div>
+                        )
+                    })}
+                </div>
+                
+              </Modal.Body>
+              <Modal.Footer>
+              </Modal.Footer>
+            </Modal>
+              
+            
+
+
+            <Modal
+                show={showStreamModal}
+                onHide={() => setShowStreamModal(false)}
+                size="lg"
+                  aria-labelledby="contained-modal-title-vcenter"
+                  centered
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter" className="w-full">
+                  <div className="font-semibold text-3xl text-center">
+                    {multiStreamValue.title}
+                </div>
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form>                                
+                    <Form.Group className="mb-3 text-xl">
+                        <Form.Label><b>Enter Stream-Key </b></Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter SECRET Key"
+                            onChange={(e) => handleChange(e)}
+                            style={{width:"80%"}}
+                        />
+                        <div className="py-2 pt-3 pl-1">
+                            RTMP : {multiStreamValue.rtmp+StreamKey}
+                        </div>
+                        
+                        <div style={{display:"flex"}}>
+                            <Button variant="primary" className=" border-0 bg-gradient-to-r from-dbeats-secondary-light to-dbeats-light rounded px-4 py-2" 
+                                type="button" 
+                                onClick={()=>{
+                                    let rtmp=multiStreamValue.rtmp+StreamKey;
+                                    createMultiStream(rtmp)
+                                }
+                            >
+                               <div className="text-white text-lg font-semibold"> 
+                                    Add {multiStreamValue.title}
+                                </div>
+                            </Button>
+
+                            <div className={classes.multistream_form_spinner}>
+                                <Spinner animation="border"  variant="info" role="status" hidden={loader}>   
+                                </Spinner>
+                            </div>
+                        </div>
+                    </Form.Group>
+                </Form>
+                
+              </Modal.Body>
+              <Modal.Footer>
+              </Modal.Footer>
+            </Modal>
+            
         </Fragment>
     );
 };
