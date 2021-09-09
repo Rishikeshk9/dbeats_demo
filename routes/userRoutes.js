48bfd9f71c71b14e92ecc7557139fa256b70a9e0
Sahil Punjabi <sahilpanjab123@gmail.com>
2021-09-09 17:26:26 +0530

    Follow Button Completed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -26,9 +26,9 @@ router.route("/add").post((req, res) => {
     id: userId,
     name: fullName,
     wallet_id: walletID,
-    livepeer_data:livepeerData,
-    password:password,
-    confirm_password:confirmPassword
+    livepeer_data: livepeerData,
+    password: password,
+    confirm_password: confirmPassword
   });
 
   newUser
@@ -36,115 +36,178 @@ router.route("/add").post((req, res) => {
     .then(() => res.json(newUser))
     .catch((err) => {
       //console.log(err);
-      res.status(400).json("Error: " + err)});
+      res.status(400).json("Error: " + err)
+    });
 });
 
-router.route("/login").post( async (req,res)=>{
-  try{
-      const username = req.body.username;
-      const password = req.body.password;
+router.route("/login").post(async (req, res) => {
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
 
-      const user_username = await User.findOne({username:username});
-      const isMatch = bcrypt.compare(password, user_username.password);
+    const user_username = await User.findOne({ username: username });
+    const isMatch = bcrypt.compare(password, user_username.password);
 
-      if(isMatch){ 
-          res.send(user_username);
-      }else{
-          res.send(false);
-      }
+    if (isMatch) {
+      res.send(user_username);
+    } else {
+      res.send(false);
     }
-  catch(error){
-      res.status(400).send("Invalid Login");
+  }
+  catch (error) {
+    res.status(400).send("Invalid Login");
   }
 })
 
-router.route("/subscribe").post( async (req,res)=>{
-  try{
+router.route("/subscribe").post(async (req, res) => {
+  try {
     const subscriberName = req.body.name;
     const subscriberUsername = req.body.username;
     const toSubscribeName = req.body.video_name;
     const toSubscribeUsername = req.body.video_username;
-    
+
     const subscribers = {
-      name:toSubscribeName, username: toSubscribeUsername,
+      name: toSubscribeName, username: toSubscribeUsername,
     };
 
     User.findOneAndUpdate(
-      {username:subscriberUsername }, 
-      { $push: { subscribers: subscribers} },
-     function (error, success) {
-           if (error) {
-               console.log(error);
-           } else {
-               console.log(success);
-           }
-        });
+      { username: subscriberUsername },
+      { $push: { subscribers: subscribers } },
+      function (error, success) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(success);
+        }
+      });
 
-        const subscribed = {
-        name:subscriberName, username: subscriberUsername,
-      };
-  
-      User.findOneAndUpdate(
-        {username: toSubscribeUsername }, 
-        { $push: { subscribed: subscribed} },
-        function (error, success) {
-             if (error) {
-                 console.log(error);
-             } else {
-                 console.log(success);
-             }
-        });
+    const subscribed = {
+      name: subscriberName, username: subscriberUsername,
+    };
+
+    User.findOneAndUpdate(
+      { username: toSubscribeUsername },
+      { $push: { subscribed: subscribed } },
+      function (error, success) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(success);
+        }
+      });
   }
-  catch(err){
+  catch (err) {
     res.send("Try Again");
   }
 })
 
-router.route("/:username").get( async (req,res)=>{
-  try{
+router.route("/:username").get(async (req, res) => {
+  try {
     const getuserData = req.params.username;
-    
-    const userData=await User.findOne({username:getuserData });
+
+    const userData = await User.findOne({ username: getuserData });
     res.send(userData)
   }
-  catch(err){
+  catch (err) {
     res.send("Try Again");
   }
 })
 
-router.route("/get_user_by_id/:streamID").get( async (req,res)=>{
-  try{
+router.route("/get_user_by_id/:streamID").get(async (req, res) => {
+  try {
     const stream_id = req.params.streamID;
 
-    const userData=await User.findOne({'livepeer_data.id':stream_id });
+    const userData = await User.findOne({ 'livepeer_data.id': stream_id });
     res.send(userData)
   }
-  catch(err){
+  catch (err) {
     res.send("Try Again");
   }
 })
 
-router.route("/getuser_by_wallet/:walletId").get( async (req,res)=>{
-  try{
+router.route("/getuser_by_wallet/:walletId").get(async (req, res) => {
+  try {
     const wallet_id = req.params.walletId;
 
-    const userData=await User.findOne({'wallet_id':wallet_id });
+    const userData = await User.findOne({ 'wallet_id': wallet_id });
     res.send(userData)
   }
-  catch(err){
+  catch (err) {
     //console.log(err)
     res.send("Try Again");
   }
 })
 
-router.route("/add_multistream_platform").post(async(req,res)=>{
-  try{
+router.route("/add_multistream_platform").post(async (req, res) => {
+  try {
     const data = req.body.multistream_data;
     const username = req.body.username
     console.log(data);
     console.log(username);
-  }catch(err){
+  } catch (err) {
     res.send("multistream_error");
+  }
+})
+
+router.route("/follow").post(async (req, res) => {
+  try {
+    const following = req.body.following;
+    const follower = req.body.follower;
+    User.findOneAndUpdate(
+      { username: following},
+      {$push: { follower_count: follower }},
+      function (error, success) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(success);
+        }
+      }
+    );
+    User.findOneAndUpdate(
+      { username: follower},
+      { $push: {followee_count: following} },
+      function (error, success) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(success);
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+router.route("/unfollow").post(async (req, res) => {
+  try {
+    const following = req.body.following;
+    const follower = req.body.follower;
+    User.findOneAndUpdate(
+      { username: following},
+      {$pull: { follower_count: follower }},
+      function (error, success) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(success);
+        }
+      }
+    );
+    User.findOneAndUpdate(
+      { username: follower},
+      { $pull: {followee_count: following} },
+      function (error, success) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(success);
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
   }
 })
 
