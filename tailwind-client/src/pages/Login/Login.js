f8655b96f00ebe0af9852a8029ee49f15a0389fa
Sahil Punjabi <sahilpanjab123@gmail.com>
2021-11-29 14:39:40 +0530

    UI changes of Footer and Login

--- tailwind-client/src/pages/Login/Login.js
+++ tailwind-client/src/pages/Login/Login.js
@@ -153,7 +153,7 @@ const Login = () => {
                 })`}
           </p>
           <img
-            className="w-12 h-12 rounded-full self-center"
+            className="2xl:w-12 2xl:h-12 lg:w-9 lg:h-9 rounded-full self-center"
             alt="metamask button"
             src="https://raw.githubusercontent.com/MetaMask/brand-resources/master/SVG/metamask-fox.svg"
           ></img>
@@ -167,7 +167,7 @@ const Login = () => {
     return (
       <div>
         <Button
-          className="font-bold flex self-center text-center"
+          className="font-bold flex lg:text-sm 2xl:text-lg self-center text-center"
           type="button"
           onClick={async () => {
             let variable = await loadWeb3Modal();
@@ -185,7 +185,7 @@ const Login = () => {
         >
           <p className="mt-2">{!provider ? 'Login using ' : `MetaMask Connected (Click Again)`}</p>
           <img
-            className="w-12 h-12 rounded-full self-center"
+            className="2xl:w-12 2xl:h-12 lg:w-9 lg:h-9 rounded-full self-center"
             alt="metamask login"
             src="https://raw.githubusercontent.com/MetaMask/brand-resources/master/SVG/metamask-fox.svg"
           ></img>
@@ -229,34 +229,34 @@ const Login = () => {
   return (
     <>
       <div className={`${darkMode && 'dark'} `}>
-        <div className="bg-gradient-to-b from-blue-50 via-blue-50 to-white  dark:bg-gradient-to-b dark:from-dbeats-dark-secondary  dark:to-dbeats-dark-primary pt-18">
+        <div className="bg-gradient-to-b from-blue-50 via-blue-50 to-white  dark:bg-gradient-to-b dark:from-dbeats-dark-secondary  dark:to-dbeats-dark-primary 2xl:pt-18 lg:pt-5">
           <main className={` w-1/2 self-center mx-auto mt-24 `}>
             <div
-              className={`py-10 px-8 bg-white dark:bg-dbeats-dark-alt w-1/2   mx-auto     self-center py-5`}
+              className={`2xl:py-10 2xl:px-8 lg:px-3 lg:py-3 bg-white dark:bg-dbeats-dark-alt w-1/2   mx-auto     self-center 2xl:py-5 lg:py-3`}
             >
               {login ? (
                 <div className="  w-full transition-all">
                   <div className="flex flex-col justify-center   text-lg px-5">
-                    <div className="self-center  text-2xl font-bold text-gray-900 dark:text-white">
+                    <div className="self-center  2xl:text-2xl lg:text-lg font-bold text-gray-900 dark:text-white">
                       SIGN UP
                     </div>
 
                     <input
-                      className="self-center my-2 rounded w-full mx-5    border-0   dark:bg-dbeats-dark-primary bg-gray-100 text-gray-900 dark:text-white focus:ring-dbeats-light"
+                      className="self-center my-2 rounded w-full mx-5  lg:h-8 2xl:h-10   border-0   dark:bg-dbeats-dark-primary bg-gray-100 text-gray-900 lg:text-xs 2xl:text-lg dark:text-white focus:ring-dbeats-light"
                       type="text"
                       placeholder="Name"
                       onChange={(e) => handleNameChange(e)}
                       required
                     />
                     <input
-                      className="self-center my-2 rounded w-full mx-5    border-0   dark:bg-dbeats-dark-primary bg-gray-100 text-gray-900 dark:text-white focus:ring-dbeats-light"
+                      className="self-center my-2 rounded w-full mx-5  lg:h-8 2xl:h-10   border-0   dark:bg-dbeats-dark-primary bg-gray-100 text-gray-900 lg:text-xs 2xl:text-lg dark:text-white focus:ring-dbeats-light"
                       type="text"
                       placeholder="Username"
                       onChange={(e) => handleUsernameChange(e)}
                       required
                     />
                     <input
-                      className="self-center my-2 rounded w-full mx-5    border-0   dark:bg-dbeats-dark-primary bg-gray-100 text-gray-900 dark:text-white focus:ring-dbeats-light"
+                      className="self-center my-2 rounded w-full mx-5  lg:h-8 2xl:h-10   border-0   dark:bg-dbeats-dark-primary bg-gray-100 text-gray-900 lg:text-xs 2xl:text-lg dark:text-white focus:ring-dbeats-light"
                       type="password"
                       placeholder="Password"
                       onChange={(e) => handlePasswordChange(e)}
@@ -264,7 +264,7 @@ const Login = () => {
                     />
                     <>
                       <input
-                        className={`self-center mt-2 mb-1 rounded w-full mx-5 
+                        className={`self-center mt-2 mb-1 rounded w-full mx-5 lg:h-8 2xl:h-10 lg:text-xs 2xl:text-lg
                         border-0  dark:bg-dbeats-dark-primary focus:outline-none
                         ${confirmPasswordCheck ? 'focus:ring-red-800' : 'focus:ring-dbeats-light'} 
                         bg-gray-100 text-gray-900 
@@ -278,18 +278,20 @@ const Login = () => {
                       />
                       <p
                         className={`${
-                          confirmPasswordCheck ? 'text-sm text-red-500 mb-1' : 'hidden'
+                          confirmPasswordCheck
+                            ? '2xl:text-sm lg:text-xs  text-red-500 mb-1'
+                            : 'hidden'
                         }`}
                       >
                         Confirm Password doesn't match Password
                       </p>
                     </>
                     <div
-                      className="flex justify-center mt-6 mb-2 cursor-pointer text-yellow-600 
+                      className="flex justify-center 2xl:mt-6 lg:mt-3 mb-2 cursor-pointer text-yellow-600 
                     border border-yellow-600 bg-white dark:bg-dbeats-dark-secondary   
                     rounded hover:bg-yellow-600 dark:hover:bg-yellow-600 
                     dark:hover:bg-opacity-5 hover:bg-opacity-5 transform transition-all 
-                    hover:scale-99  mx-3 py-1"
+                    hover:scale-99  mx-3 py-1 2xl:text-lg lg:text-sm"
                     >
                       <div className=" ">
                         <WalletButton
@@ -307,8 +309,8 @@ const Login = () => {
                           form_username === '' ||
                           form_password === '' ||
                           form_confirmPassword === ''
-                            ? 'flex justify-center  w-full  mx-3   flex my-3 py-2 px-24  text-center text-dbeats-light dark:text-white font-bold bg-dbeats-light bg-opacity-5 rounded relative cursor-default'
-                            : 'flex justify-center  w-full  mx-3   flex my-3 py-2 px-24  text-center text-dbeats-light dark:text-white font-bold bg-dbeats-light bg-opacity-5 hover:text-white hover:bg-dbeats-light border transition-all border-dbeats-light hover:scale-99 transform rounded relative'
+                            ? 'flex justify-center  w-full  mx-3   flex my-3 py-2 2xl:px-24 lg:px-10 2xl:text-lg lg:text-sm text-center text-dbeats-light dark:text-white font-bold bg-dbeats-light bg-opacity-5 rounded relative cursor-default'
+                            : 'flex justify-center  w-full  mx-3   flex my-3 py-2 2xl:px-24 lg:px-10 2xl:text-lg lg:text-sm text-center text-dbeats-light dark:text-white font-bold bg-dbeats-light bg-opacity-5 hover:text-white hover:bg-dbeats-light border transition-all border-dbeats-light hover:scale-99 transform rounded relative'
                         }`}
                         onClick={createStream}
                         disabled={
@@ -330,13 +332,13 @@ const Login = () => {
                 </div>
               ) : (
                 <div className="  w-full  ">
-                  <div className="flex flex-col justify-center   text-lg px-5">
-                    <h1 className="self-center  text-2xl font-bold text-gray-900 dark:text-white">
+                  <div className="flex flex-col justify-center   2xl:text-lg lg:texr-md px-5">
+                    <h1 className="self-center  2xl:text-2xl lg:text-md font-bold text-gray-900 dark:text-white">
                       SIGN IN
                     </h1>
                     <>
                       <input
-                        className={`self-center mt-2 mb-1 rounded w-full mx-5 
+                        className={`self-center mt-2 mb-1 rounded w-full mx-5 lg:h-8 2xl:h-10 lg:text-xs 2xl:text-lg
                         border-0  dark:bg-dbeats-dark-primary
                         ${invalidUsername ? 'border-2 border-red-500 focus:ring-red-800' : ''} 
                         bg-gray-100 text-gray-900 
@@ -347,13 +349,17 @@ const Login = () => {
                         placeholder="Username"
                         onChange={(e) => handleUsernameChange(e)}
                       />
-                      <p className={`${invalidUsername ? 'text-sm text-red-500 mb-1' : 'hidden'}`}>
+                      <p
+                        className={`${
+                          invalidUsername ? '2xl:text-sm lg:text-xs  text-red-500 mb-1' : 'hidden'
+                        }`}
+                      >
                         Please Enter Valid Username
                       </p>
                     </>
                     <>
                       <input
-                        className={`self-center mt-2 mb-1 rounded w-full mx-5 
+                        className={`self-center mt-2 mb-1 rounded w-full mx-5 lg:h-8 2xl:h-10 lg:text-xs 2xl:text-lg
                       border-0 dark:bg-dbeats-dark-primary 
                       ${invalidPassword ? 'border-2 border-red-500 focus:ring-red-800' : ''} 
                       bg-gray-100 text-gray-900 
@@ -363,7 +369,11 @@ const Login = () => {
                         placeholder="Password"
                         onChange={(e) => handlePasswordChange(e)}
                       />
-                      <p className={`${invalidPassword ? 'text-sm text-red-500 mb-1' : 'hidden'}`}>
+                      <p
+                        className={`${
+                          invalidPassword ? '2xl:text-sm lg:text-xs text-red-500 mb-1' : 'hidden'
+                        }`}
+                      >
                         Please Enter Valid Password
                       </p>
                     </>
@@ -371,7 +381,7 @@ const Login = () => {
                     <div className="flex justify-center ">
                       <button
                         onClick={handleLogin}
-                        className="flex justify-center w-full  mx-3   flex my-3 py-2 px-24  text-dbeats-light 
+                        className="flex justify-center w-full  mx-3   flex my-3 py-2 2xl:px-24 lg:px-10 2xl:text-lg lg:text-sm  text-dbeats-light 
                         dark:text-white font-bold bg-dbeats-light bg-opacity-5 
                         hover:text-white hover:bg-dbeats-light border  
                         transition-all border-dbeats-light hover:scale-99 transform rounded relative"
@@ -383,13 +393,16 @@ const Login = () => {
                         ></div>
                       </button>
                     </div>
-                    <a className="self-center py-2 text-gray-900 dark:text-white" href="/#">
-                      Forgot your password?
+                    <a
+                      className="self-center 2xl:py-2 lg:py-0 2xl:text-lg lg:text-xs text-gray-900 dark:text-white"
+                      href="/#"
+                    >
+                      Forgot your password ?
                     </a>
-                    <hr className="my-3 w-2/3 self-center" />
+                    <hr className="2xl:my-3 lg:my-2 w-2/3 self-center" />
 
                     <div
-                      className="flex justify-center mt-6 py-2 cursor-pointer text-yellow-600 border border-yellow-600 
+                      className="flex justify-center 2xl:mt-6 lg:mt-2 py-2 cursor-pointer text-yellow-600 border border-yellow-600 
                     bg-white dark:bg-dbeats-dark-secondary 
                     rounded hover:bg-yellow-600 dark:hover:bg-yellow-600 dark:hover:bg-opacity-5 
                     hover:bg-opacity-5 transform transition-all hover:scale-99"
@@ -406,11 +419,11 @@ const Login = () => {
                 </div>
               )}
             </div>
-            <div className="text-center p-5 text-gray-900 dark:text-white">
+            <div className="text-center 2xl:p-5 lg:p-2 text-gray-900 dark:text-white">
               {login ? (
                 <div>
                   <button
-                    className="rounded text-dbeats-light dark:text-gray-100 font-semibold"
+                    className="rounded text-dbeats-light lg:text-xs 2xl:text-lg dark:text-gray-100 font-semibold"
                     id="signIn"
                     onClick={handleSignIn}
                   >
@@ -420,7 +433,7 @@ const Login = () => {
               ) : (
                 <div>
                   <button
-                    className="rounded text-dbeats-light dark:text-gray-100  font-semibold"
+                    className="rounded text-dbeats-light lg:text-xs 2xl:text-lg dark:text-gray-100  font-semibold"
                     id="signUp"
                     onClick={handleSignUp}
                   >
@@ -429,12 +442,12 @@ const Login = () => {
                 </div>
               )}
             </div>
-            <div className="self-center text-center mt-5 dark:text-gray-500 font-semibold opacity-50">
+            <div className="self-center text-center 2xl:mt-5 lg:mt-0 2xl:text-lg lg:text-sm dark:text-gray-500 font-semibold opacity-50">
               powered by{' '}
               <img
                 src={moralisLogo}
                 alt="moralisLogo"
-                className="h-10 rounded w-max  self-center mx-auto bg-blue-50 dark:bg-white p-2 dark:bg-opacity-75"
+                className="2xl:h-10 lg:h-8 rounded w-max  self-center mx-auto bg-blue-50 dark:bg-white p-2 dark:bg-opacity-75"
               ></img>
             </div>
           </main>
