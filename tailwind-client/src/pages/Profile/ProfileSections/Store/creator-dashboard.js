c5b2cd70f7b38e5360530e396856c9c5f407f1eb
Rishikeshk9 <rushikeshkardile9@gmail.com>
2021-12-13 00:55:48 +0530

    NFT Listing Bug Fixes

--- tailwind-client/src/pages/Profile/ProfileSections/Store/creator-dashboard.js
+++ tailwind-client/src/pages/Profile/ProfileSections/Store/creator-dashboard.js
@@ -6,13 +6,12 @@ import { useHistory } from 'react-router-dom';
 
 //import { useRouter } from 'next/router';
 import Web3Modal from 'web3modal';
-
-const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0');
-
 import { nftaddress, nftmarketaddress } from '../config';
 
 import NFT from '../../../../artifacts/contracts/NFT.sol/NFT.json';
 import Market from '../../../../artifacts/contracts/Market.sol/NFTMarket.json';
+
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0');
 
 export default function CreateItem() {
   const [fileUrl, setFileUrl] = useState(null);
